8f9f82de870d5d8c3a04286268d7dcd8e3635b87
necrobsd <necrobsd@gmail.com>
2017-11-20 20:00:20 +1000

    проверка есть ли пользователи для подписки в аккаунте

--- follow.py
+++ follow.py
@@ -8,7 +8,7 @@ from datetime import datetime
 users_for_following = []
 api.login()
 while True:
-    target_account = 'push_up_khv' #input('Введите название аккаунта в Инстаграме: ')
+    target_account = input('Введите название аккаунта в Инстаграме: ')
     try:
         api.searchUsername(target_account)
         target_account_id = api.LastJson['user']['pk']
@@ -27,18 +27,20 @@ for follower in followers:
         users_for_following.append(follower)
         if len(users_for_following) >= REQUESTS_NUMBER:
             break
-for count, user in enumerate(users_for_following, start=1):
-    api.follow(user['pk'])
-    print(api.LastJson)
-    if api.LastJson['status'] == 'fail':
-        log(f'Ошибка при обращении к сервису Инстаграм: {api.LastJson["message"]}')
-        break
-    else:
-        print(f'{count:3}    Подписываемся на {user["username"]}')
-        User.create(uid=user['pk'],
-                    username=user['username'],
-                    full_name=user['full_name'],
-                    following_date=datetime.now())
+if users_for_following:
+    for count, user in enumerate(users_for_following, start=1):
+        api.follow(user['pk'])
+        if api.LastJson['status'] == 'fail':
+            log(f'Ошибка при обращении к сервису Инстаграм: {api.LastJson["message"]}')
+            break
+        else:
+            print(f'{count:3}    Подписываемся на {user["username"]}')
+            User.create(uid=user['pk'],
+                        username=user['username'],
+                        full_name=user['full_name'],
+                        following_date=datetime.now())
+else:
+    print('Отсутствуют пользователи, на которых можно подписаться')
 print()
 print('=' * 80, '\n')
 log(f'Отправлено новых заявок на подписку: {count}')
